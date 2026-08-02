9808bf04409f094807cfa019cd362d15d1335ae6
Ivan <hermes-cron@example.com>
2026-08-02 16:40:00 +0700

    auto-save: hotkeys.py [2026-08-02 16:39]

--- hotkeys.py
+++ hotkeys.py
@@ -312,7 +312,7 @@ def handle_key(e, st):
             if st.follow_mode:
                 st.cam_x = st.sel_cell.x
                 st.cam_y = st.sel_cell.y
-    elif k == pygame.K_F:
+    elif k == pygame.K_f:
         st.follow_mode = False
         if st.sel_cell is not None:
             st.cam_x, st.cam_y = st.sel_cell.x, st.sel_cell.y
