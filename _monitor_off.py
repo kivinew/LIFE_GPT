b68c895561eeb06871fc3fdeeb3b2aebbd1a1990
Ivan <hermes-cron@example.com>
2026-08-30 19:00:54 +0700

    auto-save: _monitor_off.py [2026-08-30 19:00]

--- _monitor_off.py
+++ _monitor_off.py
@@ -1,7 +1,8 @@
-import ctypes
+import ctypes, sys
 user32 = ctypes.windll.user32
 HWND_BROADCAST = 0xFFFF
 WM_SYSCOMMAND = 0x0112
 SC_MONITORPOWER = 0xF170
+# 2 = turn off monitor
 result = user32.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2)
-print("Monitor off:", result)
+print(f"Monitor off result: {result}", file=sys.stderr)
